3c792b7139feecd7259f55efc55d92dd763eaa2c
Jaydee P <jaydee.phanoukoun@gmail.com>
2026-04-06 20:55:07 +0200

    debug: log session content

--- frontend/app/middleware/AdminMiddleware.php
+++ frontend/app/middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ class AdminMiddleware
 {
     public static function check()
     {
+         error_log(print_r($_SESSION, true));
         if (!isset($_SESSION['user']) || !isset($_SESSION['token'])) {
             redirect('/UpcycleConnect-PA2526/frontend/public/login');
         }

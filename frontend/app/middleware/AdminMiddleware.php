4bc9c4333235b54e2b91dc9e1b6f7eeed86d5f8e
Jaydee PHANOUKOUN <jaydee.phanoukoun@gmail.com>
2026-04-06 21:22:44 +0200

    fix: check session user only

--- frontend/app/middleware/AdminMiddleware.php
+++ frontend/app/middleware/AdminMiddleware.php
@@ -5,8 +5,7 @@ class AdminMiddleware
 {
     public static function check()
     {
-         error_log(print_r($_SESSION, true));
-        if (!isset($_SESSION['user']) || !isset($_SESSION['token'])) {
+        if (!isset($_SESSION['user'])) {
             redirect('/UpcycleConnect-PA2526/frontend/public/login');
         }
     }

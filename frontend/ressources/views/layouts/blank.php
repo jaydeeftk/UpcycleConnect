00c10e504d5566701ff6a119454af32ac32a2495
Jaydee P <jaydee.phanoukoun@gmail.com>
2026-04-07 14:33:56 +0200

    fix problems

---
+++ frontend/ressources/views/layouts/blank.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <title>Maintenance - UpcycleConnect</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body class="bg-base-200">
+    <main><?= $content ?? '' ?></main>
+</body>
+</html>

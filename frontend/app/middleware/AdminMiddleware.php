37c730ed28af3b427ba97fd80964ef95c3564270
Jaydee PHANOUKOUN <jaydee.phanoukoun@gmail.com>
2026-04-07 11:26:02 +0200

    fix access admin, 403

--- frontend/app/middleware/AdminMiddleware.php
+++ frontend/app/middleware/AdminMiddleware.php
@@ -5,8 +5,18 @@ class AdminMiddleware
 {
     public static function check()
     {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
         if (!isset($_SESSION['user'])) {
             redirect('/UpcycleConnect-PA2526/frontend/public/login');
+            exit();
+        }
+
+        if ($_SESSION['user']['role'] !== 'admin') {
+            echo view('errors.403'); 
+            exit(); 
         }
     }
 }

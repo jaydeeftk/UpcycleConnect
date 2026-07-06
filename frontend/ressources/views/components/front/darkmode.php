3ff3fd9e25d1d514f990571ebd6117693a531467
Musasoos <yalasofiane94@gmail.com>
2026-07-06 20:41:52 +0200

    fix: darkmode.php

---
+++ frontend/ressources/views/components/front/darkmode.php
@@ -0,0 +1,5 @@
+<button type="button" onclick="window.themeToggle && window.themeToggle()"
+        class="text-base-content/70 hover:text-primary transition-colors" title="<?= t('nav_theme_toggle', 'Basculer le thème') ?>">
+    <i class="fas fa-moon dark:hidden"></i>
+    <i class="fas fa-sun hidden dark:inline"></i>
+</button>
